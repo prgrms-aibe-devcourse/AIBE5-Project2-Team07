9f0b76c3186722cf318ea6754b44c3cf8c962ea2
0cha0cha <taeju1013@gamil.com>
2026-04-15 10:24:54 +0900

    first commit

---
+++ src/main/java/com/example/aibe5_project2_team7/Aibe5Project2Team7Application.java
@@ -0,0 +1,13 @@
+package com.example.aibe5_project2_team7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aibe5Project2Team7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aibe5Project2Team7Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/aibe5_project2_team7/Aibe5Project2Team7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aibe5_project2_team7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aibe5Project2Team7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
