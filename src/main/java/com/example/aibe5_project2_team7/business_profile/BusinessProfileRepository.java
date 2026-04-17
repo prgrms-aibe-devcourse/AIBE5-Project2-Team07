960843f9006443f0584491b3176ec8b1e007cad8
0cha0cha <taeju1013@gamil.com>
2026-04-17 15:47:37 +0900

    [Feat] BusinessProfile 리포지토리 작성

---
+++ src/main/java/com/example/aibe5_project2_team7/business_profile/BusinessProfileRepository.java
@@ -0,0 +1,12 @@
+package com.example.aibe5_project2_team7.business_profile;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface BusinessProfileRepository extends JpaRepository<BusinessProfile, Long> {
+    @Query("SELECT bp FROM BusinessProfile bp WHERE bp.memberId = :memberId")
+    Optional<BusinessProfile> findByMemberId(@Param("memberId") Long memberId);
+}
