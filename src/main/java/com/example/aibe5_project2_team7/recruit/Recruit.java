563e0dcb8018d87169592d13bb53505dd50b5bd8
DESKTOP-QNAFNH9\leejaeseop <vavonae@gmail.com>
2026-04-17 12:47:53 +0900

    querydsl 의존성추가 querydslbeanfactory 생성 recruitentity 필요한필드만 넣기

--- src/main/java/com/example/aibe5_project2_team7/recruit/Recruit.java
+++ src/main/java/com/example/aibe5_project2_team7/recruit/Recruit.java
@@ -1,4 +1,38 @@
 package com.example.aibe5_project2_team7.recruit;
 
+import com.example.aibe5_project2_team7.recruit.constant.*;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Recruit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Boolean isUrgent;
+    @Enumerated(EnumType.STRING)
+    private BusinessTypeName businessTypeName;
+
+    @Enumerated(EnumType.STRING)
+    private Days days;
+    @Enumerated(EnumType.STRING)
+    private Period period;
+    @Enumerated(EnumType.STRING)
+    private RecruitStatus recruitStatus;
+
+    @Enumerated(EnumType.STRING)
+    private SalaryType salaryType;
+
+    @Enumerated(EnumType.STRING)
+    private Times times;
+
+
+
+
 }
