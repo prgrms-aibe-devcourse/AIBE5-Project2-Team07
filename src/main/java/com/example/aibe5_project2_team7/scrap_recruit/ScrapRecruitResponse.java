00ed68ba24310a2b7e0d8dbd4546b021255d5af6
0cha0cha <taeju1013@gamil.com>
2026-04-20 16:33:02 +0900

    [Feat] 스크랩 공고 CRD 기능 구현

--- src/main/java/com/example/aibe5_project2_team7/scrap_recruit/ScrapRecruitResponse.java
+++ src/main/java/com/example/aibe5_project2_team7/scrap_recruit/ScrapRecruitResponse.java
@@ -3,7 +3,7 @@ package com.example.aibe5_project2_team7.scrap_recruit;
 import com.example.aibe5_project2_team7.recruit.dto.RecruitListResponseDto;
 import lombok.Getter;
 
-/**
+/*
  * 스크랩 공고 응답 DTO
  * - 공고 목록 정보는 RecruitListResponseDto를 그대로 재사용
  * - 스크랩 고유 ID(scrapId)만 추가
