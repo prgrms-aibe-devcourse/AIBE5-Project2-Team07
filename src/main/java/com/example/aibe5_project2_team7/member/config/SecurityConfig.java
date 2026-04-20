68e012a585c292f005f98fc1d33da076d8bdffbd
0cha0cha <taeju1013@gamil.com>
2026-04-20 11:37:18 +0900

    [Feat] 테스트를 위한 허용 경로 추가

--- src/main/java/com/example/aibe5_project2_team7/member/config/SecurityConfig.java
+++ src/main/java/com/example/aibe5_project2_team7/member/config/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**","/personal/**","/human-resource/**").permitAll()
+                        .requestMatchers("/auth/**","/personal/**","/human-resource/**", "/recruits/**", "/business/**","/business/myrecruit/**", "/scraps/**", "/scraps/members/**", "/scraps/recruits/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(new JwtAuthenticationFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
