fe1fa73b7fc3e9c263656d7c61a173e4d11dd13a
0cha0cha <taeju1013@gamil.com>
2026-04-22 17:42:53 +0900

    [Feat] region fulladdress api 추가

---
+++ src/main/java/com/example/aibe5_project2_team7/region/RegionResponseDto.java
@@ -0,0 +1,13 @@
+package com.example.aibe5_project2_team7.region;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class RegionResponseDto {
+    private Integer id;
+    private String fullName;
+    private String sido;
+    private String sigungu;
+}
