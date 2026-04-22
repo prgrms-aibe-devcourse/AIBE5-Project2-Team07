3e4302116bab84d7363a0c3d19d3d973a0de36bb
jwh039 <jwh0393@gmail.com>
2026-04-22 14:02:34 +0900

    securityconfig & gitignore

--- src/main/java/com/example/aibe5_project2_team7/member/config/SecurityConfig.java
+++ src/main/java/com/example/aibe5_project2_team7/member/config/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**","/personal/**","/human-resource/**").permitAll()
+                        .requestMatchers("/api/auth/**","/personal/**","/human-resource/**", "/business/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(new JwtAuthenticationFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
